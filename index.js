2bf87d4c01c93fbe6cf77f69b1704220d3c0fa8d
Gurkiran kaur <gurkirank64@gmail.com>
2025-05-30 14:30:16 +0530

    project changes

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // Array to store project information
 const projects = [
     {
-        title: "Grocery App Redesign",
-        description: "A complete redesign of a grocery app, focusing on a modern and user-friendly interface.",
-        link: "https://www.figma.com/design/dLI929HwqnMmBaXgypKOVT/Grocery-App-Redesign?node-id=0-1&node-type=canvas&t=bTv60LuXWdrnBRZg-0"
-    },
-    {
         title: "Style Fusion E-Commerce Website",
         description: "An e-commerce website for men and women’s fashion, including shopping for clothes, watches, and sneakers.",
         link: "https://github.com/gurkirankaurr/Style-Fusion-ECommerce-Website"
     },
     {
-        title: "Portfolio Website",
-        description: "A personal portfolio showcasing my web development skills, projects, and achievements.",
-        link: "https://github.com/gurkirankaurr/My-portfolio"
+        title: "Mystery-block Game",
+        description: "Integrated image-based learning where users solve the puzzle by selecting the correct option. Implemented game logic to detect correct answers and trigger puzzle completion. Designed a responsive and animated UI to ensure a smooth and engaging user experience.",
+        link: "https://mystery-blocks-game-phwv79c4t-gurkirankaurrs-projects.vercel.app/"
+    },
+    {
+        title: "Task Manager Application",
+        description: "Built a task management application to streamline task organization and productivity. Features include CRUD operations for tasks, category filtering with a UI slider, and async data handling. Deployed the application on Vercel for seamless accessibility and responsiveness.",
+        link: "https://task-manager-frontend-blli6ni7c-gurkirankaurrs-projects.vercel.app/"
+    },
+    {
+        title: "Grocery App Redesign",
+        description: "A complete redesign of a grocery app, focusing on a modern and user-friendly interface.",
+        link: "https://www.figma.com/design/dLI929HwqnMmBaXgypKOVT/Grocery-App-Redesign?node-id=0-1&node-type=canvas&t=bTv60LuXWdrnBRZg-0"
     }
 ];
 
